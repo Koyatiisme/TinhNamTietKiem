422ce53542b3ac95adb465219636ae4d3775aa2b
TANDT <thangnh.pb@gmail.com>
2024-09-08 10:58:55 +0700

    Add project files.

---
+++ TinhNamTietKiem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//họ tên: Nguyễn Hữu Thắng
+//bắt đầu làm bài toán: Tính số năm cần gửi tiết kiệm
+namespace TinhNamTietKiem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
