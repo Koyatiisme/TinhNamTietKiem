d57b9824c21ea9055ca5c06825a4c2450530e8c2
TANDT <thangnh.pb@gmail.com>
2024-09-08 11:17:06 +0700

    Console app

--- TinhNamTietKiem/Program.cs
+++ TinhNamTietKiem/Program.cs
@@ -9,6 +9,24 @@ namespace TinhNamTietKiem
     {
         static void Main(string[] args)
         {
+            float s, t, p;
+            int n = 0;
+
+            Console.WriteLine("NHap so tien dang co: ");
+            s = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap so tien muon co: ");
+            t = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap lai suat % nam: ");
+            p = float.Parse(Console.ReadLine());
+
+            //Tinh toan
+            while (s<t)
+            {
+                s += s * (0.01f * p);
+                n++;
+            }
+            Console.WriteLine("So nam can gui tiet kiem = {0}", n);
+            Console.ReadKey();
         }
     }
 }
