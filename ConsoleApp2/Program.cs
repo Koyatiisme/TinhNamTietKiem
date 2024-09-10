e42e6174c6c1d80a2e223a4e5aebd8b4d762c331
TANDT <thangnh.pb@gmail.com>
2024-09-10 20:49:05 +0700

    Console2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float s, t, p;
+            int n = 0;
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Phần mềm tính số tháng cần gửi tiết kiệm");
+            Console.Write("NHap so tien dang co: ");
+            s = float.Parse(Console.ReadLine());
+            Console.Write("Nhap so tien muon co: ");
+            t = float.Parse(Console.ReadLine());
+            Console.Write("Nhap lai suat % nam: ");
+            p = float.Parse(Console.ReadLine());
+
+            //Tinh toan
+            
+        }
+
+        static bool XuLy (float s, float t, float p, ref int n)
+        {
+            while (s < t)
+            {
+                s += s * (0.01f * p);
+                n++;
+                return true;
+            }
+        }
+    }
+}
