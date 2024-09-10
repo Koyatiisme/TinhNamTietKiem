6f7956f2c4dded8253b6651d9ab50f9a597f05a1
TANDT <thangnh.pb@gmail.com>
2024-09-10 21:57:43 +0700

    pending

---
+++ WebApplication1/default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="WebApplication1._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>chương trình tính tổng hoá đơn</title>
+    <link href="StyleSheet1.css" rel="stylesheet" />
+</head>
+<body>
+    <h1>CHƯƠNG TRÌNH TÍNH SỐ THÁNG CẦN GỬI TIẾT KIỆM</h1>
+    <form id="form1" runat="server">
+        <table>
+            <tr>
+                <td class="nhan_nhap_so">Nhập số tiền hiện có: 
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="nhan_nhap_so">Nhập số tiền muốn có: 
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="nhan_nhap_so">Lãi suất(%): 
+                </td>
+                <td>
+                    <asp:TextBox ID="TextBox3" value="5" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <asp:Button ID="Button1" runat="server" Text="Tính ngay và luôn" OnClick="Button1_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <div id="ketqua" runat="server"></div>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/default.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int ok = 0;
+            try
+            {
+                float s, t, p;
+                                
+                s = float.Parse(TextBox1.Text);
+                ok = 1;
+                t = float.Parse(TextBox2.Text);
+                ok = 2;
+                p = float.Parse(TextBox3.Text) / 100;
+                ok = 3;
+
+                Lib_TinhTK.TinhSoNam thang;         
+                thang = new Lib_TinhTK.TinhSoNam();  
+                thang.s = s;                
+                thang.p = p;
+                thang.TinhThang();           
+
+                ketqua.InnerHtml = $"Tháng cần gửi tiết kiệm, áp <span title = \"lãi xuất {thang.p * 100}%\">lãi xuất {thang.p * 100}%</span> là <b class='maudo'>{thang.n}</b>";
+            }
+            catch (Exception ex)
+            {
+                if (ok == 0)
+                {
+                    ketqua.InnerHtml = $"Có lỗi khi nhập số thứ nhất";
+                    TextBox1.Focus();
+                }
+                else if (ok == 1)
+                {
+                    ketqua.InnerHtml = $"Có lỗi khi nhập số thứ hai";
+                    TextBox2.Focus();
+                }
+                else if (ok == 2)
+                {
+                    ketqua.InnerHtml = $"Có lỗi khi nhập lãi xuất";
+                    TextBox3.Focus();
+                }
+                else
+                    ketqua.InnerHtml = $"Có lỗi gì đó: " + ex.Message;
+            }
+        }
+    }
+}
