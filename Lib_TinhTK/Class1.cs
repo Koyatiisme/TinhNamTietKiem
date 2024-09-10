3d342a9b17c5cfc06ff9945886d20b725b80a0c5
TANDT <thangnh.pb@gmail.com>
2024-09-10 18:47:11 +0700

    Done console1

---
+++ Lib_TinhTK/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lib_TinhTK
+{
+    public class TinhSoNam
+    {
+        public float s, t, p;
+        public int n {  get; private set; }
+
+        public void TinhThang ()
+        {
+            while (s < t)
+            {
+                s += s * (0.01f * p);
+                n++;
+            }
+        }
+
+    }
+}
--- TinhNamTietKiem/Program.cs
+++ TinhNamTietKiem/Program.cs
@@ -11,12 +11,13 @@ namespace TinhNamTietKiem
         {
             float s, t, p;
             int n = 0;
-
-            Console.WriteLine("NHap so tien dang co: ");
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Phần mềm tính số tháng cần gửi tiết kiệm");
+            Console.Write("NHap so tien dang co: ");
             s = float.Parse(Console.ReadLine());
-            Console.WriteLine("Nhap so tien muon co: ");
+            Console.Write("Nhap so tien muon co: ");
             t = float.Parse(Console.ReadLine());
-            Console.WriteLine("Nhap lai suat % nam: ");
+            Console.Write("Nhap lai suat % nam: ");
             p = float.Parse(Console.ReadLine());
 
             //Tinh toan
